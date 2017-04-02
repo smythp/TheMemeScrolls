be8404711cab2982c189c709d584f951b42c5778
Patrick Smyth <patricksmyth01@gmail.com>
2017-04-02 15:38:20 -0400

    made a game

---
+++ dragon/dragon.py
@@ -0,0 +1,26 @@
+
+
+while True:
+    print("The Meme Scrolls [type play]")    
+    my_input = input()
+    if my_input == "play":
+        break
+    else:
+        print ("TYPE PLAY Y0U DUMBO! DID YOU NOT READ THE INSTRUCTI0NS?!?!?!")
+    
+print ("0nce in a land where there are dragons and fairies and other mythical stuff you were born and your mama kicked you out of the house and said go on an adventure and you did. you packed our meme scrolls, wings and cheetos, and other stuff.")
+
+
+
+print("You eventually are at the forest of magically wierd stoof. You  look at the trees . They look very tree-like. you wonder, where are the dragons? type one  or two to see what happens next!")
+
+answer=input()
+if answer=="one":
+    print("A dragons pops out at you. You take out the meme scrolls and show a Pokemon meme! The dragon becomes your friend. you name it Bulbasaur cuz its green and you showed it a pokemon meme.")
+elif answer=="two":
+    print("No dragons show up. you keep on walking for now.")
+else:
+    print("1 0R 2 0KAY?!?!?!") 
+
+
+    
